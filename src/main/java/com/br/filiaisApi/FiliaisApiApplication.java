2771927fab7cb12da89c1d313867cd7dc6ada007
Caroline <carolinelopesrdg@hotmail.com>
2024-06-06 16:52:21 -0300

    primeiro commit

---
+++ src/main/java/com/br/filiaisApi/FiliaisApiApplication.java
@@ -0,0 +1,13 @@
+package com.br.filiaisApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiliaisApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiliaisApiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/br/filiaisApi/domain/entity/companysBranch.java
@@ -0,0 +1,42 @@
+package com.br.filiaisApi.domain.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.br.CNPJ;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class companysBranch {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    @CNPJ
+    private String cnpj;
+
+    private String city;
+
+    private String uf;
+
+    private String type;
+
+    private Boolean active = Boolean.TRUE;
+
+    private LocalDate register;
+
+    private LocalDateTime updateTime;
+
+}
